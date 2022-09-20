0489e77c5f5f567c5555067fba98cbe9424f2155
Sheetal Jain <64475191+sheetalj2205@users.noreply.github.com>
2022-09-20 20:01:00 +0530

    Time: 331 ms (80.08%), Space: 16 MB (81.84%) - LeetHub

---
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int lcsSubarray(vector<int>& nums1, int n, vector<int>& nums2, int m){
+        int dp[n+1][m+1];
+        int result = 0;
+        for(int i = 0; i<n+1; i++){
+        for(int j  = 0; j<m+1; j++){
+            if(i == 0 || j == 0){
+                dp[i][j] = 0;
+            }
+        }
+    }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    result = max(result, dp[i][j]);
+                }
+                else{
+                    dp[i][j]  = 0;
+                }
+            }
+        }
+        return result;
+        
+    }
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 1;
+        int n = nums1.size();
+        int m = nums2.size();
+        if(n == 1 || m == 1){
+             return ans;
+         }
+        ans = lcsSubarray(nums1, n, nums2, m);
+        return ans;
+        
+    }
+};
